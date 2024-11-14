bf175bac5496a34d466ac7af2e217fcc633bb462
Rodolphe Quiedeville <r.quiedeville@smartway.ai>
2024-11-14 12:05:25 +0100

    Fix integration test for extension

--- test/extension.sql
+++ test/extension.sql
@@ -12,8 +12,8 @@ TRUNCATE ddl_history;
 CREATE EXTENSION pg_trgm;
 
 SELECT results_eq(
-    'SELECT count(*) FROM ddl_history',
-    'SELECT CAST(63 as bigint)'
+    'SELECT count(*) FROM ddl_history WHERE ddl_tag=''CREATE EXTENSION'' ',
+    'SELECT CAST(1 as bigint)'
 );
 
 
