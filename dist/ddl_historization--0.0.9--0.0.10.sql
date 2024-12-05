12eb772b515de25c5b719f53063c2daac96bc240
Rodolphe Quiedeville <r.quiedeville@smartway.ai>
2024-12-05 12:28:34 +0100

    Add permissions

--- dist/ddl_historization--0.0.9--0.0.10.sql
+++ dist/ddl_historization--0.0.9--0.0.10.sql
@@ -1,2 +1,25 @@
 CREATE UNIQUE INDEX ddl_pkey on ddl (id);
 ALTER TABLE ddl add PRIMARY KEY USING INDEX ddl_pkey;
+
+CREATE OR REPLACE FUNCTION log_ddl_auth()
+  RETURNS void AS $funky$
+DECLARE
+  owner TEXT;
+BEGIN
+
+  EXECUTE 'SELECT u.usename FROM pg_database d join pg_user u on u.usesysid=d.datdba
+WHERE datname = current_database() ' INTO owner;
+
+  EXECUTE format('GRANT SELECT,INSERT ON ddl_history TO %I', owner);
+  EXECUTE format('GRANT USAGE ON ddl_history_id_seq TO  %I', owner);
+
+  EXECUTE format('GRANT SELECT,INSERT,DELETE ON ddl_history_column TO %I', owner);
+
+  EXECUTE format('GRANT SELECT,INSERT ON ddl_history_schema TO %I', owner);
+  EXECUTE format('GRANT USAGE ON ddl_history_schema_id_seq TO  %I', owner);
+
+END;
+$funky$ LANGUAGE plpgsql;
+
+
+SELECT log_ddl_auth();
--- dist/pgtle.ddl_historization-0.0.10.sql
+++ dist/pgtle.ddl_historization-0.0.10.sql
@@ -181,10 +181,35 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 --
+-- When the extension is installed by a superuser the database owner needs rights
+--
+CREATE OR REPLACE FUNCTION log_ddl_auth()
+  RETURNS void AS $funky$
+DECLARE
+  owner TEXT;
+BEGIN
+
+  EXECUTE 'SELECT u.usename FROM pg_database d join pg_user u on u.usesysid=d.datdba
+WHERE datname = current_database() ' INTO owner;
+
+  EXECUTE format('GRANT SELECT,INSERT ON ddl_history TO %I', owner);
+  EXECUTE format('GRANT USAGE ON ddl_history_id_seq TO  %I', owner);
+
+  EXECUTE format('GRANT SELECT,INSERT,DELETE ON ddl_history_column TO %I', owner);
+
+  EXECUTE format('GRANT SELECT,INSERT ON ddl_history_schema TO %I', owner);
+  EXECUTE format('GRANT USAGE ON ddl_history_schema_id_seq TO  %I', owner);
+
+END;
+$funky$ LANGUAGE plpgsql;
+--
 -- Automatically start the historization at the end of install.
 --
 SELECT @extschema@.log_ddl_start();
 
 INSERT INTO @extschema@.ddl_history_schema (schema_name) VALUES ('public');
+
+
+SELECT @extschema@.log_ddl_auth();
 $_pg_tle_$
 );
